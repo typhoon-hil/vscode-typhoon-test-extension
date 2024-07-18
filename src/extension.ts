0173c47855165731d6c0dd09dfb2cdf9c93a7c36
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-07-18 16:37:52 +0200

    Refactor sidebar webview to display a simplified initial view

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,16 @@
 import * as vscode from 'vscode';
 import { SidebarProvider } from './SidebarProvider';
 import { TreeDataProvider } from './TreeDataProvider';
+import { showDocstringCommand } from './showDocstringCommand';
+import { TreeNode } from './TreeDataProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Extension "typhoon-test" is now active!');
-
-  let disposable = vscode.commands.registerCommand('helloworld.helloWorld', async () => {
-    await vscode.commands.executeCommand('workbench.action.files.newUntitledFile');
-    await vscode.commands.executeCommand('workbench.view.explorer');
-    await vscode.commands.executeCommand('workbench.action.terminal.toggleTerminal');
-  });
-
-  context.subscriptions.push(disposable);
-
-  const sidebarProvider = new SidebarProvider(context.extensionUri);
-  context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider('customView', sidebarProvider)
-  );
-
+  let sidebarProvider = new SidebarProvider(context.extensionUri);
+  vscode.window.registerWebviewViewProvider('customView', sidebarProvider);
   vscode.window.registerTreeDataProvider('customView2', new TreeDataProvider('typhoon.api.schematic_editor'));
+  context.subscriptions.push(vscode.commands.registerCommand('helloworld.helloWorld', (item:TreeNode) => 
+    showDocstringCommand(sidebarProvider, item)
+));
 }
 
 export function deactivate() {}
