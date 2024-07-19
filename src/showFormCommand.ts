08e52ebc44ac1c9365bcf5ef0f57c6ffbec7d55b
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-07-19 14:10:38 +0200

    refactor: Update showFormCommand

--- src/showFormCommand.ts
+++ src/showFormCommand.ts
@@ -1,7 +1,6 @@
-import * as vscode from 'vscode';
 import { FormProvider } from "./FormProvider";
 import { TreeNode } from "./TreeDataProvider";
 
 export function showFormCommand(formProvider: FormProvider, item: TreeNode) {
-    vscode.window.showInformationMessage("Showing form for method");
+    formProvider.update_html(item.args);
 }
