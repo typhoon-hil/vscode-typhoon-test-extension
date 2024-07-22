a5b24ce123cacf7aa997019a6beb94b346c1e78f
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-07-22 14:13:47 +0200

    refactor: Register module tree view for Python modules and cleanup extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,22 @@
 // extension.ts
 import * as vscode from 'vscode';
 import { SidebarProvider } from './SidebarProvider';
-import { TreeDataProvider } from './TreeDataProvider';
 import { showDocstringCommand } from './showDocstringCommand';
 import { TreeNode } from './TreeDataProvider';
 import { FormProvider } from './FormProvider';
 import { showFormCommand } from './showFormCommand';
 import { handleTreeViewItemClickedCommand } from './handleTreeViewItemClickedCommand';
+import { showApiOptionsCommand } from './showApiOptionsCommand';
+import { registerModuleTreeView } from './registerModuleTreeView';
 
 export function activate(context: vscode.ExtensionContext) {
   let sidebarProvider = new SidebarProvider(context.extensionUri);
   let formProvider = new FormProvider(context.extensionUri);
+
   vscode.window.registerWebviewViewProvider('typhoon-test.docstringView', sidebarProvider);
-  vscode.window.registerTreeDataProvider('typhoon-test.pythonModuleView', new TreeDataProvider('typhoon.api.schematic_editor'));
   vscode.window.registerWebviewViewProvider('typhoon-test.formView', formProvider);
+  registerModuleTreeView('typhoon.api.schematic_editor');
+
   context.subscriptions.push(vscode.commands.registerCommand('typhoon-test.showDocstring', (item: TreeNode) =>
     showDocstringCommand(sidebarProvider, item)
   ));
@@ -32,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(
     vscode.commands.registerCommand('typhoon-test.showApiOptions', () => {
-      vscode.window.showInformationMessage('Hello, World!');
+      showApiOptionsCommand();
     })
   );
 }
