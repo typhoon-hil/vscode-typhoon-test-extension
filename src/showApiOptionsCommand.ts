ed5f47da91c69a0c4b1fa2da32f8af0861e66d8d
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-07-22 14:13:38 +0200

    feat: Add showApiOptionsCommand to display API options

---
+++ src/showApiOptionsCommand.ts
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+import { registerModuleTreeView } from './registerModuleTreeView';
+
+
+const apiOptions: { [key: string]: string } = {
+    'Schematic API': 'typhoon.api.schematic_editor',
+    'HIL API': 'typhoon.api.hil',
+};
+
+export async function showApiOptionsCommand() {
+    const options: vscode.QuickPickItem[] = [
+        ...Object.keys(apiOptions).map(label => ({
+            label: label
+        })),
+    ];
+
+    const selectedOption = await vscode.window.showQuickPick(options, {
+        placeHolder: 'Select an API to view'
+    });
+
+    if (selectedOption) {
+        const alias = await vscode.window.showInputBox({
+            prompt: `Enter an alias for the ${selectedOption.label}:`
+        });
+
+        if (alias) {
+            registerModuleTreeView(apiOptions[selectedOption.label]);        
+        }
+    }
+}
