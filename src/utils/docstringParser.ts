dd779ed8cca854cc225251da2c92c317d6ec08f5
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-07-22 16:33:44 +0200

    refactor: Improve docstring parsing and HTML conversion

--- src/utils/docstringParser.ts
+++ src/utils/docstringParser.ts
@@ -1,8 +1,10 @@
 export function docstringToHtml(docstring: string): string {
     return `<div>${docstring.replace(/\n/g, '<br>')
         .replace(/(Args|Returns|Raises):/g, '<b>$1:</b>')
-        .replace(/^( {4}|\t)(.*)/gm, '<code>$2</code>')}</div>`;
-}
+        .replace(/^( {4}|\t)(.*)/gm, '<code>$2</code>')}</div>`
+        .replace(/\*\*(.*?)\*\*/g, '<b>$1</b>')
+        .replace(/`(.*?)`/g, '<code>$1</code>');
+    }
 
 export function getDescription(docstring: string): string {
     return docstring.split('\n')[0].trim();
