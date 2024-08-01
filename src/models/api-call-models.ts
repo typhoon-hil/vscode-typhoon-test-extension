924272d92afc4c4d0fc675d46bed410ef9bf6729
Balša Bulatović <baki.b.2k02@gmail.com>
2024-08-01 14:15:12 +0200

    feat: Create python code related types

--- src/models/api-call-models.ts
+++ src/models/api-call-models.ts
@@ -1,3 +1,7 @@
+export type PythonEntityType = "class" | "module";
+export type PythonMemberType = "method" | "function";
+export type PythonType = PythonEntityType | PythonMemberType;
+
 export interface FunctionArgument {
     name: string;
     default: any;
