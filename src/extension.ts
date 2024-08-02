6d998c768372420d1aa9737890906bc734f551d5
Balša Bulatović <baki.b.2k02@gmail.com>
2024-08-02 03:34:31 +0200

    refactor: Cleanup extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,62 +1,63 @@
 // extension.ts
 import * as vscode from 'vscode';
-import { DocumentationProvider } from './views/DocumentationProvider';
-import { showDocstring } from './commands/showDocstring';
-import { ArgumentsProvider } from './views/ArgumentsProvider';
-import { showForm } from './commands/showForm';
-import { handleTreeViewItemClicked } from './commands/handleTreeViewItemClicked';
-import { showApiOptions } from './commands/showApiOptions';
-import { saveApiWizardWorkspace } from './commands/saveApiWizardWorkspace';
+import {DocumentationProvider} from './views/DocumentationProvider';
+import {showDocstringView} from './commands/showDocstringView';
+import {ArgumentsProvider} from './views/ArgumentsProvider';
+import {showArgumentsView} from './commands/showArgumentsView';
+import {handleTreeViewItemClicked} from './commands/handleTreeViewItemClicked';
+import {showApiOptions} from './commands/showApiOptions';
+import {saveApiWizardWorkspace} from './commands/saveApiWizardWorkspace';
 import {TreeNode} from "./models/TreeNode";
 import {PythonEntityTreeProvider} from "./views/PythonEntityTreeProvider";
 
 export function activate(context: vscode.ExtensionContext) {
-  let sidebarProvider = new DocumentationProvider(context.extensionUri);
-  let formProvider = new ArgumentsProvider(context.extensionUri);
+    let sidebarProvider = new DocumentationProvider(context.extensionUri);
+    let formProvider = new ArgumentsProvider(context.extensionUri);
 
-  vscode.window.registerWebviewViewProvider('typhoon-test.docstringView', sidebarProvider);
-  vscode.window.registerWebviewViewProvider('typhoon-test.formView', formProvider);
-  vscode.window.registerTreeDataProvider('typhoon-test.pythonModuleView', PythonEntityTreeProvider.getInstance());
+    vscode.window.registerWebviewViewProvider('typhoon-test.docstringView', sidebarProvider);
+    vscode.window.registerWebviewViewProvider('typhoon-test.formView', formProvider);
+    vscode.window.registerTreeDataProvider('typhoon-test.pythonModuleView', PythonEntityTreeProvider.getInstance());
 
-  context.subscriptions.push(vscode.commands.registerCommand('typhoon-test.showDocstring', (item: TreeNode) =>
-    showDocstring(sidebarProvider, item)
-  ));
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand('typhoon-test.showForm', (item: TreeNode) => {
-      showForm(formProvider, item);
-    }
+    context.subscriptions.push(vscode.commands.registerCommand('typhoon-test.showDocstring', (item: TreeNode) =>
+        showDocstringView(sidebarProvider, item)
     ));
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('typhoon-test.handleTreeViewItemClicked', (item: TreeNode) => {
-      handleTreeViewItemClicked(item);
-    }
-    ));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('typhoon-test.showArgumentsView', (item: TreeNode) => {
+                showArgumentsView(formProvider, item);
+            }
+        ));
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('typhoon-test.showApiOptions', () => {
-      showApiOptions().then();
-    })
-  );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('typhoon-test.handleTreeViewItemClicked', (item: TreeNode) => {
+                handleTreeViewItemClicked(item);
+            }
+        ));
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('typhoon-test.removeModule', (item: TreeNode) => {
-      vscode.window.showInformationMessage(`Remove ${item.label}`).then();
-    })
-  );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('typhoon-test.showApiOptions', () => {
+            showApiOptions().then();
+        })
+    );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('typhoon-test.saveApiWizardWorkspace', () => {
-      saveApiWizardWorkspace();
-    })
-  );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('typhoon-test.removeModule', (item: TreeNode) => {
+            vscode.window.showInformationMessage(`Remove ${item.label}`).then();
+        })
+    );
 
-  vscode.workspace.onDidChangeConfiguration(event => {
-    if (event.affectsConfiguration('typhoon-test.apiWizardWorkspace')) {
-      PythonEntityTreeProvider.getInstance().loadEntitiesFromWorkspace().then();
-    }
-  });
+    context.subscriptions.push(
+        vscode.commands.registerCommand('typhoon-test.saveApiWizardWorkspace', () => {
+            saveApiWizardWorkspace();
+        })
+    );
+
+    vscode.workspace.onDidChangeConfiguration(event => {
+        if (event.affectsConfiguration('typhoon-test.apiWizardWorkspace')) {
+            PythonEntityTreeProvider.getInstance().loadEntitiesFromWorkspace().then();
+        }
+    });
 }
 
-export function deactivate() { }
+export function deactivate() {
+}
