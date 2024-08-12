dbcbaaf07967604bc975f24421b336598c8265d9
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-08-12 11:36:51 +0200

    feat: Create test run config getters

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,9 @@
 import vscode from 'vscode';
-import {PythonImport} from "../models/pythonEntity";
+import { PythonImport } from "../models/pythonEntity";
+import { PythonInterpreterType, TestRunConfig } from '../models/testRun';
 
 const config = vscode.workspace.getConfiguration('typhoon-test');
+const testRunConfig = vscode.workspace.getConfiguration('typhoon-test.testRun');
 
 export function loadWorkspaceElements(): PythonImport[] {
     return config.get<PythonImport[]>('apiWizardWorkspace', []);
@@ -14,4 +16,58 @@ export async function saveWorkspaceElements(elements: PythonImport[]) {
 export function getLineSpacing(): string {
     const lineSpacingCount = config.get<number>('lineSpacing')!;
     return '\n'.repeat(lineSpacingCount);
+}
+
+export function getTestRunConfig(): TestRunConfig {
+    return {
+        pythonInterpreterType: getPythonInterpreterType(),
+        customPythonInterpreterPath: getCustomPythonInterpreterPath(),
+        realTimeLogs: getRealTimeLogs(),
+        openReport: getOpenReport(),
+        cleanOldResults: getCleanOldResults(),
+        pdfReport: getPdfReport(),
+        selectTestByName: getSelectTestByName(),
+        selectTestByMark: getSelectTestByMark(),
+        additionalOptions: getAdditionalOptions()
+    };
+}
+
+function getPythonInterpreterType(): PythonInterpreterType {
+    return testRunConfig.get<PythonInterpreterType>('pythonInterpreterType')!;
+}
+
+function getCustomPythonInterpreterPath(): string | undefined {
+    let path = testRunConfig.get<string | undefined>('customPythonInterpreterPath');
+    return path && path.trim() ? path : undefined;
+}
+
+function getRealTimeLogs(): boolean {
+    return testRunConfig.get<boolean>('realTimeLogs', false);
+}
+
+function getOpenReport(): boolean {
+    return testRunConfig.get<boolean>('openReport', true);
+}
+
+function getCleanOldResults(): boolean {
+    return testRunConfig.get<boolean>('cleanOldResults', false);
+}
+
+function getPdfReport(): boolean {
+    return testRunConfig.get<boolean>('pdfReport', false);
+}
+
+function getSelectTestByName(): string | undefined {
+    let name = testRunConfig.get<string | undefined>('selectTestByName');
+    return name && name.trim() ? name : undefined;
+}
+
+function getSelectTestByMark(): string | undefined {
+    let mark = testRunConfig.get<string | undefined>('selectTestByMark');
+    return mark && mark.trim() ? mark : undefined;
+}
+
+function getAdditionalOptions(): string | undefined {
+    let options = testRunConfig.get<string | undefined>('additionalOptions');
+    return options && options.trim() ? options : undefined;
 }
