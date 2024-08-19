f41c52cdd57069716497061bfa58a3ffe1b1cce1
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-08-19 14:31:42 +0200

    feat: Add TestItem class for displaying test status in the tree view

---
+++ src/models/testMonitoring.ts
@@ -0,0 +1,26 @@
+import * as vscode from 'vscode';
+
+export enum TestStatus {
+    Running = 'running',
+    Passed = 'passed',
+    Failed = 'failed',
+}
+
+export class TestItem extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+        public readonly status: TestStatus,
+    ) {
+        super(label, collapsibleState);
+        this.description = status;
+        this.setIcon();
+    }
+
+    private setIcon(): void {
+        this.iconPath = new vscode.ThemeIcon(this.status === 'running' ? 'sync~spin' : 
+            this.status === 'passed' ? 'check' : 
+            'error'
+        );
+    }
+}
