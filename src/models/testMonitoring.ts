3215cb27c5d6ea71b17148ca7d963cd1ccefd787
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-08-20 14:25:14 +0200

    refactor: Update TestItem class to support additional test statuses
    This commit modifies the TestItem class in the testMonitoring.ts file to support additional test statuses: XFailed and XPassed. By adding these statuses, the TestItem class can now properly display the corresponding icons in the tree view, enhancing the visual representation of test statuses. This refactor improves the flexibility and completeness of the test monitoring functionality.

--- src/models/testMonitoring.ts
+++ src/models/testMonitoring.ts
@@ -4,6 +4,9 @@ export enum TestStatus {
     Running = 'running',
     Passed = 'passed',
     Failed = 'failed',
+    XFailed = 'xfailed',
+    Skipped = 'skipped',
+    XPassed = 'xpassed',
 }
 
 export class TestItem extends vscode.TreeItem {
@@ -17,9 +20,28 @@ export class TestItem extends vscode.TreeItem {
     }
 
     private setIcon(): void {
-        this.iconPath = new vscode.ThemeIcon(this.status === 'running' ? 'sync~spin' : 
-            this.status === 'passed' ? 'check' : 
-            'error'
-        );
+        switch (this.status) {
+            case TestStatus.Running:
+                this.iconPath = new vscode.ThemeIcon('sync~spin');
+                break;
+            case TestStatus.Passed:
+                this.iconPath = new vscode.ThemeIcon('check');
+                break;
+            case TestStatus.Failed:
+                this.iconPath = new vscode.ThemeIcon('error');
+                break;
+            case TestStatus.XFailed:
+                this.iconPath = new vscode.ThemeIcon('warning');
+                break;
+            case TestStatus.Skipped:
+                this.iconPath = new vscode.ThemeIcon('circle-outline');
+                break;
+            case TestStatus.XPassed:
+                this.iconPath = new vscode.ThemeIcon('warning');
+                break;
+            default:
+                this.iconPath = new vscode.ThemeIcon('question');
+                break;
+        }
     }
 }
