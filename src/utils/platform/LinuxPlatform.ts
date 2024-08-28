a5bb00ca5614301886258714887aff2e6412edfb
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-08-28 16:01:37 +0200

    refactor: Rename getEmbeddedPythonPath to getEmbeddedPythonCommand in Platform interface and its implementations

--- src/utils/platform/LinuxPlatform.ts
+++ src/utils/platform/LinuxPlatform.ts
@@ -1,26 +1,15 @@
-import * as path from 'path';
 import { Platform } from '../../models/Platform';
-import { findLatestTyphoonPath } from '.';
 
 export class LinuxPlatform implements Platform {
     getPythonCommand(): string {
         return 'python3';
     }
 
-    getEmbeddedPythonPath(): string {
-        const typhoonPaths = this.extractTyphoonPaths();
-        const selectedPath = findLatestTyphoonPath(typhoonPaths);
-        const cleanedPath = selectedPath.replace(/\/bin\/$/, '');
-        return selectedPath ? path.join(cleanedPath, 'python3_portable', 'python3') : '';
+    getEmbeddedPythonCommand(): string {
+        return 'typhoon-python';
     }
 
     getPythonExecutableExtension(): string {
         return '';
-    }
-
-    private extractTyphoonPaths(): string[] {
-        const pathEnv = process.env.PATH || '';
-        const pathDirs = pathEnv.split(':');
-        return Array.from(new Set(pathDirs.filter(dir => dir.includes('Typhoon'))));
     }
 }
--- src/utils/platform/WindowsPlatform.ts
+++ src/utils/platform/WindowsPlatform.ts
@@ -1,26 +1,15 @@
-import * as path from 'path';
 import { Platform } from '../../models/Platform';
-import { findLatestTyphoonPath } from '.';
 
 export class WindowsPlatform implements Platform {
     getPythonCommand(): string {
         return 'python';
     }
 
-    getEmbeddedPythonPath(): string {
-        const typhoonPaths = this.extractTyphoonPaths();
-        const selectedPath = findLatestTyphoonPath(typhoonPaths);
-        const cleanedPath = selectedPath.replace(/\\bin\\$/, '');
-        return selectedPath ? path.join(cleanedPath, 'python3_portable', 'python.exe') : '';
+    getEmbeddedPythonCommand(): string {
+        return 'typhoon-python';
     }
 
     getPythonExecutableExtension(): string {
         return 'exe';
-    }
-
-    private extractTyphoonPaths(): string[] {
-        const pathEnv = process.env.PATH || '';
-        const pathDirs = pathEnv.split(';');
-        return Array.from(new Set(pathDirs.filter(dir => dir.includes('Typhoon'))));
     }
 }
