33d119414d22950719796fd19e475f0085e65ca6
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-08-29 14:35:26 +0200

    fix: Built-in methods causing ValueError

--- scripts/get_module_source.py
+++ scripts/get_module_source.py
@@ -1,8 +1,8 @@
-import sys
 import inspect
+import sys
 import importlib
 import json
-from utils import *
+from utils import is_public, get_doc, get_parameters
 
 
 def get_methods_from_module(module_name):
@@ -11,21 +11,26 @@ def get_methods_from_module(module_name):
         module = importlib.import_module(module_name)
     except ModuleNotFoundError:
         return json.dumps({"error": f"Module '{module_name}' not found."})
-    
+
     # Dictionary to store all functions
     functions_data = {"type": "module", "name": module_name, "callables": []}
 
     # Iterate over all the members of the module
     for name, obj in inspect.getmembers(module):
-        if (inspect.isfunction(obj) or inspect.isbuiltin(obj)) and is_public(name):
-            function_info = {
-                "name": name,
-                "doc": get_doc(obj),
-                "args": get_parameters(obj)
-            }
-            functions_data["callables"].append(function_info)
+        if inspect.isfunction(obj) and is_public(name) and obj.__module__ == module_name:
+            try:
+                function_info = {
+                    "name": name,
+                    "doc": get_doc(obj),
+                    "args": get_parameters(obj)
+                }
+                functions_data["callables"].append(function_info)
+            except ValueError:
+                # Skip functions that do not have a signature (e.g., built-in functions)
+                continue
 
     return json.dumps(functions_data, indent=2)
+
 
 if __name__ == "__main__":
     # Check if the module name is provided as an argument
