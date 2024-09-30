bb93adbd9a5cc790bc5d3cfb1086e04aa5cc32c2
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-09-30 17:14:09 +0200

    refactor: Update error handling in PythonEntityTreeProvider class

--- src/utils/pythonConverter.ts
+++ src/utils/pythonConverter.ts
@@ -10,12 +10,8 @@ export async function loadPythonEntity(pythonCommand: string, moduleName: string
 async function extractPython(pythonCommand: string, moduleName: string, type: PythonEntityType): Promise<string> {
     return new Promise((resolve, reject) => {
         const filePath = path.resolve(__dirname, '..', '..', 'scripts', `get_${type}_source.py`);
-        exec(`${pythonCommand} ${filePath} ${moduleName}`, (error, stdout, stderr) => {
-            if (error) {
-                reject(`Error loading ${type}: ${stderr}, ${error}`);
-            } else {
-                resolve(stdout);
-            }
+        exec(`${pythonCommand} ${filePath} ${moduleName}`, (error, stdout, _) => {
+            return error ? reject(error) : resolve(stdout);
         });
     });
 }
